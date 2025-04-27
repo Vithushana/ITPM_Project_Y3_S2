3f090b3007a09e935cdc1731b499d33f75760f8a
IT22182128 <IT22182128@my.sliit.lk>
2025-04-27 16:41:18 +0530

    inventory control

--- be/src/main/java/com/home_zone/home_zone/controller/InventoryController.java
+++ be/src/main/java/com/home_zone/home_zone/controller/InventoryController.java
@@ -1,7 +1,6 @@
 package com.home_zone.home_zone.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
