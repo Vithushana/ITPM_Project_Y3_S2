ed87f494db0e7d46152136876a16497422186465
IT22182128 <IT22182128@my.sliit.lk>
2025-04-27 21:20:45 +0530

    fixed chaanges

--- be/src/main/java/com/home_zone/home_zone/model/InventoryItem.java
+++ be/src/main/java/com/home_zone/home_zone/model/InventoryItem.java
@@ -6,7 +6,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "inventoryitems")
 public class InventoryItem {
     @Id 
-    
     private String id;
     private String name;
     private int quantity;
